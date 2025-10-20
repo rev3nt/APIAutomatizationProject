4e065771088ef686313feac638fae64fc32b1cbe
rev3nt <mikhail.merkulov.2005@mail.ru>
2025-10-20 15:19:55 +0300

    Added browser open test, also test_results dir added for allure reports

--- tests/test_open_browser.py
+++ tests/test_open_browser.py
@@ -4,21 +4,34 @@ from selenium.webdriver.common.by import By
 import time
 
 
+# Тест для открытия ссылки в браузере
 def test_open_browser():
+    # URL сайта, на который необходимо зайти
     url = 'https://vkusnoitochka.ru'
 
+    # Настраиваем браузер таким образом, чтобы он сразу не закрывался после исполнения скрипта
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
 
-    driver = webdriver.Chrome()
+    # Создаем драйвер, загружаем в него настройки
+    driver = webdriver.Chrome(options=options)
+
+    # Открываем ссылку в браузере и разворачиваем его на полный экран
     driver.get(url)
     driver.maximize_window()
 
-    success_text = driver.find_element(By.XPATH, '//h1[contains(text(), "Приходи к нам!")]').text
-    expected_text = 'Приходи к нам!'
+    # Создаем текст, который должен отображаться на открытой странице
+    expected_text = 'Приложение'
+    # Находим локатор нужного элемента и извлекаем из него текст
+    success_text = driver.find_element(By.XPATH, f'//p[contains(text(), {expected_text})]').text
+    print(success_text)
 
+    # Добавляем проверку, что текст соответствует ожидаемому результату
     assert success_text == expected_text
+    print("Сайт был успешно открыт!")
 
+    # Добавляем небольшое ожидание
     time.sleep(3)
 
+    # Закрываем браузер
     driver.quit()
